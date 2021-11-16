f5f0dc60b4e117b782bf02bcf82d4feedf6720ed
sghoang24 <hoangnguyennho24@gmail.com>
2021-11-16 15:20:50 +0700

    test push

--- TestKhachHang.cpp
+++
@@ -1,9 +0,0 @@
-#include <string> 
-#include <iostream>
-using namespace std;
-int main(){
-    string x = "40";
-    // std::string s = std::to_string(x);
-    int n = stoi(x);
-    std::cout << n;
-}
