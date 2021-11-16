de6a3aa4385748f9de9de3504f89e2cfdb0e83fb
LamLe <lenguyenngoclam1@gmail.com>
2021-11-16 15:44:22 +0700

    Fix main #2

--- test.cpp
+++ test.cpp
@@ -6,27 +6,9 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
-    // DanhSachNhanVien ds;
-    // ds.CaiDatDanhSach();
-    // ds.InDanhSach();
-
-    // Node<NhanVien>* current = ds.getHead();
-    // (current -> getData()).DoiThongTinCaNhan();
-    // cout << "------------Danh sach moi------------" << endl;
-    // ds.InDanhSach();
-
-    NhanVien nv;
-    nv.CaiDatThongTin();
-    nv.LayThongTinCaNhan();
-    // ds.SuaDanhSach(nv);
-    // cout << "------------Danh sach moi------------" << endl;
-    // ds.InDanhSach();
-=======
     TaiKhoanNhanVien temp("ngoclam3092002","3092002");
     DanhSachTaiKhoan ds;
     ds.themTaiKhoan(&temp);
     ds.head -> data -> getInfo(cout);
->>>>>>> main
     return 0;
 }
